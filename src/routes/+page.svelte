06a8ba9fce09ed4d3455028f80491b1ecabcb0f5
ksmai <kasingmai@gmail.com>
2023-03-26 11:20:09 +0800

    feat: add meta tags in <svelte:head>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,31 @@
   import AnimationOptions from "$lib/components/AnimationOptions.svelte";
   import Animations from "$lib/components/Animations.svelte";
   import Preview from "$lib/components/Preview.svelte";
+
+  const title = "Jumping Peter";
+  const description =
+    "With Jumping Peter you can generate custom GIF emojis from images, with dozens of pre-built animations and effects";
+  const url = "https://ksmai.github.io/jumping-peter";
+  const image = "https://ksmai.github.io/jumping-peter/favicon.png";
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="title" content={title} />
+  <meta name="description" content={description} />
+
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={url} />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta property="og:image" content={image} />
+
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content={url} />
+  <meta property="twitter:title" content={title} />
+  <meta property="twitter:description" content={description} />
+  <meta property="twitter:image" content={image} />
+</svelte:head>
 
 <div class="grid">
   <Header class="header" />
