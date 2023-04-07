1ffa5e686e30a5c6304148c9ad59da95624433d2
ksmai <kasingmai@gmail.com>
2023-04-07 04:31:35 +0000

    Deploying to gh-pages from @ ksmai/jumping-peter@7c134c6ac62160bcd4472b89aab05cafd8ac2c4e 🚀

---
+++ _app/immutable/chunks/1.ec44c899.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.356526e0.js";export{t as component};
---
+++ _app/immutable/chunks/2.f88cce43.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.753a8c19.js";export{t as component};
