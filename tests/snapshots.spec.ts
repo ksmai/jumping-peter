32dbeb6cd4ff1273b9e0fa5eed0e5285841a4aae
ksmai <kasingmai@gmail.com>
2023-04-08 19:38:16 +0800

    test: add snapshot test for animation

---
+++ tests/snapshots.spec.ts
@@ -0,0 +1,24 @@
+import { expect, test } from "@playwright/test";
+import { promises as fs } from "fs";
+
+const animationNames = ["jumping"];
+
+for (const animationName of animationNames) {
+  test(animationName, testAnimationSnapshot(animationName));
+}
+
+function testAnimationSnapshot(
+  animationName: string,
+): Parameters<typeof test>[1] {
+  return async ({ page }) => {
+    await page.goto("/");
+    const downloadPromise = page.waitForEvent("download");
+    await page.getByRole("button", { name: animationName }).click();
+    await page.getByRole("link", { name: "Download" }).click();
+    const download = await downloadPromise;
+    const path = await download.path();
+    expect(path).toBeTruthy();
+    const content = await fs.readFile(path!);
+    expect(content).toMatchSnapshot();
+  };
+}
