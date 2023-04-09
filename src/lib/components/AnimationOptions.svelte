bc0707f80017c43c4adb2b70689fde4c62d71c5b
ksmai <kasingmai@gmail.com>
2023-04-09 16:02:35 +0800

    feat: add Section in AnimationOptions

--- src/lib/components/AnimationOptions.svelte
+++ src/lib/components/AnimationOptions.svelte
@@ -2,30 +2,37 @@
   import { animations } from "$lib/store";
   import Slider from "$lib/components/Slider.svelte";
   import Toggle from "$lib/components/Toggle.svelte";
+  import Section from "$lib/components/Section.svelte";
 
   let className = "";
   export { className as class };
 </script>
 
 <aside class={`container ${className}`}>
-  {#each $animations.current.editOptions as option}
-    {#if option.type === "slider"}
-      <Slider
-        label={option.name}
-        min={option.min}
-        max={option.max}
-        step={option.step}
-        value={option.value}
-        on:input={(e) => animations.changeEditOptions(option, e.detail.value)}
-      />
-    {:else if option.type === "toggle"}
-      <Toggle
-        label={option.name}
-        value={option.value}
-        on:change={(e) => animations.changeEditOptions(option, e.detail.value)}
-      />
-    {/if}
-  {/each}
+  <Section
+    title={$animations.current.name}
+    on:reset={animations.resetEditOptions}
+  >
+    {#each $animations.current.editOptions as option}
+      {#if option.type === "slider"}
+        <Slider
+          label={option.name}
+          min={option.min}
+          max={option.max}
+          step={option.step}
+          value={option.value}
+          on:input={(e) => animations.changeEditOptions(option, e.detail.value)}
+        />
+      {:else if option.type === "toggle"}
+        <Toggle
+          label={option.name}
+          value={option.value}
+          on:change={(e) =>
+            animations.changeEditOptions(option, e.detail.value)}
+        />
+      {/if}
+    {/each}
+  </Section>
 </aside>
 
 <style lang="scss">
